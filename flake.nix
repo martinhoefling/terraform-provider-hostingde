ea6e58a5df535140d8496e7c2c7483f048fe6ab2
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-01-25 20:06:57 +0100

    fix: make docs needs terraform CLI in PATH
    Use terraform 1.5.x, last version with open source MPL license

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "hosting.de terraform provider";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-2305.url = "github:nixos/nixpkgs/nixos-23.05";
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = inputs@{ self, flake-parts, devshell, nixpkgs }:
+  outputs = inputs@{ self, flake-parts, devshell, nixpkgs, nixpkgs-2305 }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         devshell.flakeModule
@@ -25,6 +26,7 @@
             go-tools
             gnumake
             golangci-lint
+            inputs.nixpkgs-2305.legacyPackages.${pkgs.system}.terraform
           ];
           bash.extra = ''
             export GOPATH=~/.local/share/go
